8aed7f5ea3b0f4a2c9bb20ded9bc9a9ce908a403
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2020-11-10 21:20:30 +0800

    wmc

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,60 +1,46 @@
 <template>
-	<div class="header">
-		<div class="content">
-			<div class="title">线上实验平台</div>
-			<div class="right">
-				<img
-					class="icon"
-					src="../assets/icon.png"
-					alt=""
-				>
-				<img
-					class="whutlogo"
-					src="../assets/whutlogo.png"
-					alt=""
-				>
-
-			</div>
-
-		</div>
-
-	</div>
+  <div class="header">
+    <div class="content">
+      <div class="title">线上实验平台</div>
+      <div class="right">
+        <img class="icon" src="../assets/icon.png" alt="" />
+        <img class="whutlogo" src="../assets/whutlogo.png" alt="" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
-	name: "GlobalHeader"
+  name: "GlobalHeader",
 });
 </script>
 <style>
 .header {
-	height: 50px;
- 
-
-	background-color: #418fe8;
+  height: 50px;
+  background-color: #418fe8;
 }
 .content {
-	width: 1200px;
-	height: 100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin: auto;
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
 }
 
 .title {
-	font-size: 28px;
-	color: #ffffff;
+  font-size: 28px;
+  color: #ffffff;
 }
 .icon {
-	/* margin-left: 543.75px; */
-	width: 35px;
-	height: 35px;
+  width: 35px;
+  height: 35px;
 }
 
 .whutlogo {
-	width: 195px;
-	height: 44px;
+  width: 195px;
+  height: 44px;
 }
 </style>
