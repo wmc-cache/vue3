3524abcfb2deba41e14ef581bf9fd51ce079954e
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2020-11-15 20:43:25 +0800

    wmc

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,50 +1,67 @@
 <template>
-	<div>
-		<a-upload
-			action="http://localhost:3000/upload"
-			list-type="picture"
-			v-model:fileList="fileList"
-			@change="change"
-		>
-			<a-button>
-				<upload-outlined /> 上传图片 </a-button>
-		</a-upload>
-
-	</div>
+<div>
+    <a-upload action="https://www.mocky.io/v2/5cc8019d300000980a055e76" list-type="picture" v-model:fileList="fileList">
+        <a-button>
+            <upload-outlined /> 上传图片
+        </a-button>
+    </a-upload>
+    <pre>{{ fileList }}</pre>
+    <button @click="fabu">fabu</button>
+</div>
 </template>
 
 <script lang="ts">
-import { UploadOutlined } from "@ant-design/icons-vue";
-import { defineComponent, PropType, computed, ref, watch } from "vue";
+import {
+    UploadOutlined
+} from "@ant-design/icons-vue";
+import {
+    defineComponent,
+    PropType,
+    computed,
+    ref,
+    watch
+} from "vue";
 export default defineComponent({
-	components: {
-		UploadOutlined
-	},
-	setup() {
-    const fileList = ref(null);
-   
-		const change = e => {
-      console.log(typeof e)
-      // if(Array.isArray(e)){
-
-	       console.log(e.fileList);
-      // }
-		
-		}
-		return { fileList, change };
-	}
+    components: {
+        UploadOutlined,
+    },
+    setup() {
+        const fileList = ref();
+        // watch(fileList, () => {
+        //     if (fileList.value[0].response) {
+        //         console.log(fileList.value[0].response.url);
+        //     }
+        // });
+        // computed(() => {
+        //     if (fileList.value[0].response) {
+        //         console.log(fileList.value[0].response.url);
+        //     }
+        // });
+        const fabu = () => {
+            fileList.value.forEach((ele: any) => {
+                console.log(ele.response.url);
+            });
+        };
+        return {
+            fileList,
+            fabu
+        };
+    },
 });
 </script>
+
 <style scoped>
 .upload-list-inline ::v-deep(.ant-upload-list-item) {
-	float: left;
-	width: 200px;
-	margin-right: 8px;
+    float: left;
+    width: 200px;
+    margin-right: 8px;
 }
+
 .upload-list-inline ::v-deep(.ant-upload-animate-enter) {
-	animation-name: uploadAnimateInlineIn;
+    animation-name: uploadAnimateInlineIn;
 }
+
 .upload-list-inline ::v-deep(.ant-upload-animate-leave) {
-	animation-name: uploadAnimateInlineOut;
+    animation-name: uploadAnimateInlineOut;
 }
 </style>
--- src/views/Violent.vue
+++ src/views/Violent.vue
@@ -1,67 +1,78 @@
 <template>
-  <div class="violent">
+<div class="violent">
     <Header></Header>
     <Tab></Tab>
 
     <div class="content">
-      <div class="body">
-        <push v-if="display"></push>
-        <div class="area">
-          <router-view> </router-view>
+        <div class="body">
+            <push v-if="display"></push>
+            <div class="area">
+                <router-view> </router-view>
+            </div>
         </div>
-      </div>
 
-      <div class="tipArea">
-        <Tip></Tip>
-      </div>
+        <div class="tipArea">
+            <Tip></Tip>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
-import { useStore } from "vuex";
-import Header from "@/views/violent/Header";
-import Tab from "@/views/violent/Tab";
-import Tip from "@/views/violent/Tip";
-import Push from "@/views/violent/Push";
+import {
+    defineComponent,
+    computed
+} from "vue";
+import {
+    useStore
+} from "vuex";
+import Header from "@/views/violent/Header.vue";
+import Tab from "@/views/violent/Tab.vue";
+import Tip from "@/views/violent/Tip.vue";
+import Push from "@/views/violent/Push.vue";
 export default defineComponent({
-  name: "Violent",
-  components: {
-    Header,
-    Tab,
-    Tip,
-    Push,
-  },
-  setup() {
-    const store = useStore();
-    const display = computed(() => store.state.pushEventDialog);
-    return { display };
-  },
+    name: "Violent",
+    components: {
+        Header,
+        Tab,
+        Tip,
+        Push,
+    },
+    setup() {
+        const store = useStore();
+        const display = computed(() => store.state.pushEventDialog);
+        return {
+            display
+        };
+    },
 });
 </script>
 
 <style scoped>
 .violent {
-  position: relative;
+    position: relative;
 }
+
 .content {
-  width: 1200px;
+    width: 1200px;
 }
+
 .body {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
+
 .area {
-  margin-top: 20px;
-  width: 837px;
-  box-shadow: 0px 3px 18px rgba(42, 59, 84, 0.12);
-  opacity: 1;
-  border-radius: 10px;
+    margin-top: 20px;
+    width: 837px;
+    box-shadow: 0px 3px 18px rgba(42, 59, 84, 0.12);
+    opacity: 1;
+    border-radius: 10px;
 }
+
 .tipArea {
-  position: absolute;
-  right: 0;
-  top: 20px;
+    position: absolute;
+    right: 0;
+    top: 20px;
 }
 </style>
